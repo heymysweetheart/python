b96e31d0affd50e2f4fe454da093c56a4ad736bd
yuliang.jin <yuliang.jin@qunar.com>
2015-03-09 01:12:07 +0800

    complete python basic examples

--- pythonbasics/HelloPython.py
+++ pythonbasics/HelloPython.py
@@ -57,3 +57,104 @@ print("new_A=", new_A)
 print("new_B=", new_B)
 
 print(make_them_different(3,3))
+
+#files
+test_file = open("test.txt", "wb")
+print(test_file.mode)
+print(test_file.name)
+test_file.write(bytes("Write me to the file", "UTF-8"))
+test_file.close()
+
+file_for_read = open("test.txt", "r+")
+text_in_file = file_for_read.read()
+print(text_in_file)
+
+os.remove("test.txt")
+
+#python class
+class Animal:
+    __name = None
+    __height= 0
+    __weight= 0
+    __sound= 0
+
+    def __init__(self, name, height, weight, sound):
+        self.__name = name
+        self.__height = height
+        self.__weight = weight
+        self.__sound = sound
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return  self.__name
+
+    def set_height(self, height):
+        self.__height = height
+
+    def get_height(self):
+        return  self.__height
+
+    def set_weight(self, weight):
+        self.__weight = weight
+
+    def get_weight(self):
+        return  self.__weight
+
+    def set_sound(self, sound):
+        self.__sound = sound
+
+    def get_sound(self):
+        return  self.__sound
+
+    def get_type(self):
+        print("Animal")
+
+    def toString(self):
+        return "{} is {} cm tall and {} kg weight and say {} .".format(self.get_name(),
+                                                                    self.get_height(),
+                                                                    self.get_weight(),
+                                                                    self.get_sound())
+
+# initialization of the class
+cat = Animal("tom", 10, 2, "meow")
+print(cat.toString())
+print(cat.get_height())
+
+# inheritage
+class Dog(Animal):
+    __owner = ""
+
+    def __init__(self, name, height, weight, sound, owner):
+        self.__owner = owner
+        super(Dog, self).__init__(name, height, weight, sound)
+
+    def set_owner(self, owner):
+        self.__owner = owner
+
+    def get_owner(self):
+        return self.get_owner()
+
+    def get_type(self):
+        print("Dog")
+
+    def multi_sound(self, sound_time = None):
+        if(sound_time == 0):
+            print(self.get_sound())
+        else:
+            print(self.get_sound() * sound_time)
+
+spot = Dog("hero", 12, 20, "wang", "leo")
+print(spot.toString())
+# print(spot.get_type())
+
+class AnimalTest:
+    def get_type(self, animal):
+        return animal.get_type()
+
+test_animals= AnimalTest()
+test_animals.get_type(cat)
+test_animals.get_type(spot)
+
+spot.multi_sound(4)
