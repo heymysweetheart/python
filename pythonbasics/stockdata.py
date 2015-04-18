091dd064fdc76dc03b51107f5e09db4c1b2bdc5b
yuliang <zhixiangjin@126.com>
2015-04-18 18:24:33 +0800

    use regex to represent a stock compayn' ;

--- pythonbasics/stockdata.py
+++ pythonbasics/stockdata.py
@@ -9,9 +9,11 @@ symble = "AAPL"
 symblelist = ["AAPL","SPY","GOOG","NFLX"]
 for symb in symblelist:
     url = "http://finance.yahoo.com/q?s=" + symb
-    regex = '<span id="yfs_l84_[*]">(.+?)</span>'
-    htmltext = urllib.request.urlopen(url).read()
-    price = re.findall('<span id="yfs_l84_' + symb + '">(.+?)</span>', htmltext)
-
-    print("price of " + symb + " is: " + price[0].decode("utf-8"))
+    htmltext = urllib.request.urlopen(url).read() # read() return a bytes objects
+    #print(htmltext) #print a bytes object, result would be like this: b'<!DOCTYPE html PUBLIC "-//W3
+    pattern = '<span id="yfs_l84_(.{1,6})">(.+?)</span>'
+    #pattern = '<span id="yfs_l84_' + symb.lower() + '">(.+?)</span>'
+    price = re.findall(pattern, str(htmltext))
+    print(price)
+    #print("price of " + symb + " is: " + price[0].decode("utf-8"))
     # todo read param from file and write results to files.
