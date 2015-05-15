077d9934c6b5b623b8d7e5a9e372710dea49512a
yuliang <zhixiangjin@126.com>
2015-05-15 10:27:03 +0800

    add regular expression examples

--- pythonbasics/pyregex.py
+++ pythonbasics/pyregex.py
@@ -5,9 +5,9 @@ import urllib.request
 import re
 
 '''
-This code is to get a stock index of some companies from yahoo finance.
+This code is to how to use regular expression in python.
 '''
-symblelist = ["AAPL","SPY","GOOG","NFLX"]
+symblelist = ["AAPL","SPY"]
 for symb in symblelist:
     url = "http://finance.yahoo.com/q?s=" + symb
     htmltext = urllib.request.urlopen(url).read() # read() return a bytes objects
@@ -16,6 +16,9 @@ for symb in symblelist:
     #bellow pattern could also work well
     #pattern = '<span id="yfs_l84_' + symb.lower() + '">(.+?)</span>'
     price = re.findall(pattern, str(htmltext))
+    matches = re.search(pattern,str(htmltext))
     print(price)
+    print(matches.group(1))
+    print(matches.group(2))
     #print("price of " + symb + " is: " + price[0].decode("utf-8"))
     # todo read param from file and write results to files.
